b22a619e590cfb6c4b8b94b5cf6a6bf087984741
Spratley, Jonathan Patrick <Jonnie.Spratley@ge.com>
2014-07-02 12:33:33 -0700

    updated files

--- app/index.js
+++ app/index.js
@@ -1,94 +1,101 @@
-(function() {
-  'use strict';
-  var JsblogGenerator, chalk, path, util, yeoman;
+(function () {
+	'use strict';
+	var util = require( 'util' );
+	var path = require( 'path' );
+	var yeoman = require( 'yeoman-generator' );
+	var yosay = require( 'yosay' );
+	var MyGenerator;
 
-  util = require('util');
+	module.exports = MyGenerator = yeoman.generators.Base.extend();
+	MyGenerator.prototype.init = function() {
+		this.pkg = require('../package.json');
+		this.on('end', function() {
+			if (this.options['skip-install'] !== true) {
+				this.installDependencies();
+			}
+		});
+	};
+	MyGenerator.prototype.askFor = function() {
+		var done = this.async();
 
-  path = require('path');
+		this.log( this.yeoman );
+		this.log( yosay( 'You are using JPS Site Yeoman generator.' ) );
+		this.prompts = [
+			{
+				type: 'input',
+				name: 'siteTitle',
+				message: 'What is the name of your site',
+				"default": 'My Site'
+			}, {
+				type: 'input',
+				name: 'siteDesc',
+				message: 'What is the site description?',
+				"default": 'A modern site built with a Yeoman Generator.'
+			}, {
+				type: 'input',
+				name: 'featureTitle',
+				message: 'What is the feature?',
+				"default": 'Modern Site'
+			}, {
+				type: 'input',
+				name: 'featureBody',
+				message: 'The feature description?',
+				"default": 'A modern site using modern tools & technologies.'
+			}, {
+				type: 'input',
+				name: 'featureImage',
+				message: 'The feature image?',
+				"default": 'images/feature.png'
+			}
+		];
+		this.prompt(this.prompts, (function(props) {
+			this.siteTitle = props.siteTitle;
+			this.siteDesc = props.siteDesc;
+			this.featureTitle = props.featureTitle;
+			this.featureBody = props.featureBody;
+			this.featureImage = props.featureImage;
+			done();
+		}).bind(this));
+	};
 
-  yeoman = require('yeoman-generator');
+	MyGenerator.prototype.projectfiles = function () {
+		this.copy('_package.json', 'package.json');
+		this.copy('_Gruntfile.js', 'Gruntfile.js');
 
-  chalk = require('chalk');
+		//Copy all of the bower specific files.
+		this.copy('bowerrc', '.bowerrc');
+		this.copy('_bower.json', 'bower.json');
 
-  module.exports = JsblogGenerator = yeoman.generators.Base.extend();
+		//Copy all files that handle code editing.
+		this.copy('editorconfig', '.editorconfig');
+		this.copy('jshintrc', '.jshintrc');
 
-  JsblogGenerator.prototype.init = function() {
-    this.on('end', function() {
-      if (!this.options['skip-install']) {
-        return this.installDependencies();
-      }
-    });
-    return this.pkg = require('../package.json');
-  };
+		//Copy all files that handle git repositorys
+		this.copy('gitignore', '.gitignore');
+		this.copy('gitattributes', '.gitattributes');
 
-  JsblogGenerator.prototype.askFor = function() {
-    var done, prompts;
-    done = this.async();
-    this.log(this.yeoman);
-    this.log(chalk.yellow('You are using the JS Blog Yeoman generator.'));
-    prompts = [
-      {
-        type: 'input',
-        name: 'blogName',
-        message: 'What is the name of your blog?',
-        "default": 'My Blog'
-      }, {
-        type: 'input',
-        name: 'featureTitle',
-        message: 'What is the feature?',
-        "default": 'Modern Blog'
-      }, {
-        type: 'input',
-        name: 'featureDesc',
-        message: 'The feature description?',
-        "default": 'A modern blog using modern tools & technologies.'
-      }, {
-        type: 'input',
-        name: 'featureImg',
-        message: 'The feature image?',
-        "default": 'images/feature.png'
-      }
-    ];
-    return this.prompt(prompts, (function(props) {
-      this.blogName = props.blogName;
-      this.featureTitle = props.featureTitle;
-      this.featureDesc = props.featureDesc;
-      this.featureImg = props.featureImg;
-      return done();
-    }).bind(this));
-  };
+		//Copy files for Travis CI.
+		this.copy('travis.yml', '.travis.yml');
+	};
 
-  JsblogGenerator.prototype.appFolders = function() {
-    this.mkdir('app');
-    this.mkdir('app/images');
-    this.mkdir('app/scripts');
-    return this.mkdir('app/styles');
-  };
-
-  JsblogGenerator.prototype.appFiles = function() {
-    this.copy('feature.png', 'app/images/feature.png');
-    this.template('_index.html', 'app/index.html');
-    this.template('_main.js', 'app/scripts/main.js');
-    return this.template('_main.css', 'app/styles/main.css');
-  };
-
-  JsblogGenerator.prototype.projectfiles = function() {
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('jshintrc', '.jshintrc');
-    this.template('_config.json', 'config.json');
-    this.template('_package.json', 'package.json');
-    return this.template('_Gruntfile.js', 'Gruntfile.js');
-  };
-
-  JsblogGenerator.prototype.bowerFiles = function() {
-    this.template('_bower.json', 'bower.json');
-    return this.copy('bowerrc', '.bowerrc');
-  };
-
-  JsblogGenerator.prototype.bowerInstaller = function() {
-    return this.bowerInstall(['jquery', 'bootstrap'], {
-      save: true
-    });
-  };
+	MyGenerator.prototype.app = function () {
+		this.mkdir('app');
+		this.mkdir('app/images');
+		this.mkdir('app/scripts');
+		this.mkdir('app/styles');
+		this.mkdir('app/pages');
+		this.copy('feature.png', 'app/images/feature.png');
+		this.copy('_index.html', 'app/index.html');
+		this.copy('_main.html', 'app/pages/main.html');
+		this.copy('_main.js', 'app/scripts/main.js');
+		this.copy('_main.css', 'app/styles/main.css');
+	};
+	MyGenerator.prototype.bowerInstaller = function () {
+		if (this.options['skip-install'] !== true) {
+			this.bowerInstall(['jquery', 'handlebars', 'bootstrap'], {
+				save: true
+			});
+		}
+	};
 
 }).call(this);
--- test/test-creation.js
+++ test/test-creation.js
@@ -1,30 +1,79 @@
-/*global describe, beforeEach, it */'use strict';
+'use strict';
 var path = require('path');
 var helpers = require('yeoman-generator').test;
+require('chai').should();
 
-describe('jsblog generator', function() {
+
+describe('jps-site generator', function() {
+
+	//Mocked questions and answers
+	var mockAnswers = {
+		siteTitle: 'My Test Site',
+		siteDesc: 'A modern site build to test.',
+		featureTitle: 'Mocha Tests',
+		featureBody: 'A modern site created by a Yeoman generator.',
+		featureImage: 'images/feature.png'
+	};
+
+	//Mock expected files
+	var mockFiles = [
+		'.bowerrc',
+		'.editorconfig',
+		'.gitattributes',
+		'.gitignore',
+		'.jshintrc',
+		'.travis.yml',
+		'bower.json',
+		'Gruntfile.js',
+		'package.json',
+		'app/images/feature.png',
+		'app/styles/main.css',
+		'app/scripts/config.js',
+		'app/scripts/main.js',
+		'app/index.html'
+	];
+
+	//Mock expected file contents
+	var mockFilePairs = [
+		[ 'app/index.html', RegExp("<title>" + mockAnswers.siteTitle + "</title>") ],
+		[ 'app/pages/main.html', RegExp("<h1>" + mockAnswers.featureTitle + "</h1>") ],
+		[ 'app/pages/main.html', RegExp("" + mockAnswers.featureBody) ]
+	];
+
+	/**
+	 Before each test clean the test/temp folder and create a new generator.
+	 */
 	beforeEach(function(done) {
-		helpers.testDirectory(path.join(__dirname, 'temp'), function(err) {
+		helpers.testDirectory(path.join(__dirname, 'temp'), (function(err) {
 			if (err) {
-				return done(err);
+				done(err);
 			}
-
-			this.app = helpers.createGenerator('jsblog:app', ['../../app']);
+			this.app = helpers.createGenerator('jps-site:app', ['../../app']);
 			done();
-		}.bind(this));
+		}).bind(this));
 	});
 
+	/**
+	 First test if the files that were created match what we expect.
+	 */
 	it('creates expected files', function(done) {
-		var expected = [
-		// add files you expect to exist here.
-		'.jshintrc', '.editorconfig'];
 
-		helpers.mockPrompt(this.app, {
-			'someOption' : true
-		});
+		/**
+		 Add some mock prompts for the user to answer
+		 */
+		helpers.mockPrompt(this.app, mockAnswers);
+
+		/**
+		 Skip installing of the bower and npm dependencies
+		 */
 		this.app.options['skip-install'] = true;
+
+		/**
+		 Run the app and test for files and file contents
+		 */
 		this.app.run({}, function() {
-			helpers.assertFile(expected);
+			helpers.assertFile(mockFiles);
+			helpers.assertFileContent(mockFilePairs);
 			done();
 		});
 	});
